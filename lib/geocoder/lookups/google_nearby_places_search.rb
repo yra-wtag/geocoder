11e9084d188b0f9836abf19d806146ad39802dc5
Yeasin Ar Rahman <yeasin.ar_rahman@welldev.io>
2020-03-16 16:56:23 +0600

    completed nearby places implementation

--- lib/geocoder/lookups/google_nearby_places_search.rb
+++ lib/geocoder/lookups/google_nearby_places_search.rb
@@ -16,6 +16,19 @@ module Geocoder
         [:https]
       end
 
+      def search(query, options = {})
+        query = Geocoder::Query.new(query, options) unless query.is_a?(Geocoder::Query)
+        api_output = results(query)
+        {
+          next_page: api_output["next_page_token"],
+          results: api_output['results'].map{ |r|
+            result = result_class.new(r)
+            result.cache_hit = @cache_hit if cache
+            result
+          }
+        }
+      end
+
       private
 
       def base_query_url(query)
@@ -29,6 +42,24 @@ module Geocoder
             type: "lodging",
             language: query.language || configuration.language
         }
+      end
+
+      def results(query)
+        return [] unless doc = fetch_data(query)
+        case doc['status'];
+        when "OK" # OK status implies >0 results
+          return doc
+        when "OVER_QUERY_LIMIT"
+          raise_error(Geocoder::OverQueryLimitError) ||
+              Geocoder.log(:warn, "#{name} API error: over query limit.")
+        when "REQUEST_DENIED"
+          raise_error(Geocoder::RequestDenied, doc['error_message']) ||
+              Geocoder.log(:warn, "#{name} API error: request denied (#{doc['error_message']}).")
+        when "INVALID_REQUEST"
+          raise_error(Geocoder::InvalidRequest, doc['error_message']) ||
+              Geocoder.log(:warn, "#{name} API error: invalid request (#{doc['error_message']}).")
+        end
+        return []
       end
     end
   end
